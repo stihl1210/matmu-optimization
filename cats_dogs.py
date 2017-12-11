374d63036274e33feac9fde3309ee45e5f69ebad
r.pilarczyk <r.pilarczyk@samsung.com>
2017-12-11 11:25:40 +0100

    model and cats_dogs

--- cats_dogs.py
+++ cats_dogs.py
@@ -1,57 +1,28 @@
+import PIL
+
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.models import load_model
-import matplotlib.pyplot as plt
 
-datagen = ImageDataGenerator(
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest')
+import numpy as np
 
-img = load_img('PetImages/Cat/2.jpg')
-x = img_to_array(img)
-print(x.shape)
-x = x.reshape((1,) + x.shape)
+import scipy
 
-import os
-directory_test = './test-generator2'
+def deprocess_image(path):
+    x = np.asarray(PIL.Image.open(path))
+    x = scipy.misc.imresize(x, (350,350))
+    # x = np.divide(x, 255.0)
+    # x = np.subtract(x, 1.0)
+    # x = np.multiply(x, 2.0)
+    x = np.expand_dims(x, axis=0)
+    print(x.shape)
+    return x
 
-if not os.path.exists(directory_test):
-    os.makedirs(directory_test)
+model = load_model('best_model.h5')
 
-i = 0
+img = deprocess_image('./train/dog.66.jpg')
 
-for batch in datagen.flow(x, batch_size=1,
-                          save_to_dir=directory_test,
-                          save_prefix='cat',
-                          save_format='jpg'):
-    i += 1
-    if i > 20:
-        break
+pred = model.predict(img)
 
-names = [na for na in os.listdir(directory_test)]
-fig = plt.figure()
-for img_pil in names:
-    img = load_img(os.path.join(directory_test, img_pil))
-    x = img_to_array(img)
+print(pred, 1)
 
-    ax = plt.subplot(1, 1, 1)
-    plt.tight_layout()
-    ax.set_title('Sample #{}'.format(i))
-    ax.axis('off')
-
-    plt.imshow(img)
-    plt.show()
-
-    if i == 6:
-        plt.show()
-        break
-
-model = load_model('catsdogs.h5')
-
-x = model.predict(img)
-
+# cat - 0, dog - 1
